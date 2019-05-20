2e991583e7c427f64aa526a10e565f678805af3a
Your Name <you@example.com>
2019-05-20 22:52:57 +0200

    first commit

---
+++ Maja_testiranje/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Maja_testiranje.MvcApplication" Language="C#" %>
---
+++ Maja_testiranje/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Maja_testiranje.Startup))]
+namespace Maja_testiranje
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
